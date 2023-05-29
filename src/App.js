7d920e7a2a33b77a405dac344c63f5bd3fcdd8a3
muzaffar <vojvojev@gmail.com>
2023-05-29 17:45:28 +0500

    added something

--- src/App.js
+++ src/App.js
@@ -27,8 +27,30 @@ function App() {
           <h1 className="h-font2">LUXURY</h1>
           <h2 className="h-font3">
             Book your stay and enjoy Luxury <br></br>
-            redefined at the most affordable rates.{" "}
+            redefined at the most affordable rates.
           </h2>
+        </div>
+        <div className="butn">
+          <button className="bttn">BOOK NOW</button>
+        </div>
+      </div>
+      <div className="component">
+        <div className="hassh">
+          <h1 className="h1-m">
+            All our room types are including complementary breakfast
+          </h1>
+        </div>
+        <div className="card1">
+          <div className="mini-card">
+            <h1 className="luxsury">Luxury redefined</h1>
+            <p className="p-lux">
+              Our rooms are designed to transport <br></br> you into an
+              environment made for leisure. <br></br> Take your mind off the
+              day-to-day of home <br></br> life and find a private paradise for
+              yourself.
+            </p>
+            <button className="btn-explore">EXPLORE</button>
+          </div>
         </div>
       </div>
     </div>
