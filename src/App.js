00e8e2d766a75c825b46da17237d3c636048a067
muzaffar <vojvojev@gmail.com>
2023-05-29 17:03:08 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import pic from "./uy.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box">
+        <div className="nav">
+          <img width={160} src={pic} />
+          <div className="pi-flex">
+            <a className="pi1" href="">
+              Home
+            </a>
+            <a className="pi2" href="">
+              Facilities
+            </a>
+            <a className="pi3" href="">
+              Rooms
+            </a>
+            <a className="pi4" href="">
+              Contact-us
+            </a>
+          </div>
+        </div>
+        <div className="h-work">
+          <h1 className="h-font">WELCOME TO</h1>
+          <h1 className="h-font2">LUXURY</h1>
+          <h2 className="h-font3">
+            Book your stay and enjoy Luxury <br></br>
+            redefined at the most affordable rates.{" "}
+          </h2>
+        </div>
+      </div>
     </div>
   );
 }
